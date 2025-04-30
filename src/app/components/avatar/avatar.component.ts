96af2ec793f275a4261c69a4a4856f0b3b248225
cacko <alex@cacko.net>
2025-04-30 08:06:07 +0100

    Out for vacation... DONT YOU DARE TO CALL ME.

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -2,9 +2,9 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from "@angular/fire/auth";
 import { MatRippleModule } from '@angular/material/core';
-// import multiavatar from "@multiavatar/multiavatar";
-// import md5 from 'md5';
-// const b64encode = window.btoa;
+import multiavatar from "@multiavatar/multiavatar";
+import md5 from 'md5';
+const b64encode = window.btoa;
 
 interface ImageStyle {
   [key: string]: string
@@ -29,28 +29,28 @@ export class AvatarComponent  {
   constructor() { }
 
   ngOnInit(): void {
-    // this.imageStyle = { "background-image": `url(${this.getMultiavatar()})` };
-    // const canvas = document.createElement('canvas');
-    // const image = new Image();
-    // image.setAttribute("crossorigin", "anonymous");
-    // image.src = this.user?.photoURL || "";
-    // image.addEventListener("load", () => {
-    //   canvas.width = image.naturalWidth;
-    //   canvas.height = image.naturalHeight;
-    //   canvas.getContext('2d')?.drawImage(image, 0, 0);
-    //   const data64 = canvas.toDataURL("image/jpeg", 0.9);
-    //   this.imageStyle = { "background-image": `url(${data64})` };
-    // });
+    this.imageStyle = { "background-image": `url(${this.getMultiavatar()})` };
+    const canvas = document.createElement('canvas');
+    const image = new Image();
+    image.setAttribute("crossorigin", "anonymous");
+    image.src = this.user?.photoURL || "";
+    image.addEventListener("load", () => {
+      canvas.width = image.naturalWidth;
+      canvas.height = image.naturalHeight;
+      canvas.getContext('2d')?.drawImage(image, 0, 0);
+      const data64 = canvas.toDataURL("image/jpeg", 0.9);
+      this.imageStyle = { "background-image": `url(${data64})` };
+    });
   }
 
-  // getMultiavatar(): string {
-  //   const key = md5(`${this.user?.email}${this.user?.uid}`);
-  //   let avatar = localStorage.getItem(`avatar-${key}`);
-  //   if (!avatar) {
-  //     const svg = multiavatar(key);
-  //     avatar = `data:image/svg+xml;base64,${b64encode(svg)}`;
-  //     localStorage.setItem(key, avatar);
-  //   }
-  //   return avatar || "";
-  // }
+  getMultiavatar(): string {
+    const key = md5(`${this.user?.email}${this.user?.uid}`);
+    let avatar = localStorage.getItem(`avatar-${key}`);
+    if (!avatar) {
+      const svg = multiavatar(key);
+      avatar = `data:image/svg+xml;base64,${b64encode(svg)}`;
+      localStorage.setItem(key, avatar);
+    }
+    return avatar || "";
+  }
 }
